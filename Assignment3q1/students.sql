3dcde5f8b7a2e7a66f2f582dffe90beb90323362
yamanAlrajab <112143246+yamanAlrajab@users.noreply.github.com>
2024-03-18 13:03:53 -0400

    Create students.sql

---
+++ Assignment3q1/students.sql
@@ -0,0 +1,11 @@
+create table students(
+	student_id SERIAL primary key,
+	first_name text not null,
+	last_name text not null,
+	email text not null unique,
+	enrollment_date Date
+);
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
