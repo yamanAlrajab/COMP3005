452b1219c2785b59be9e3a0f1f821e7719e5582e
yamanAlrajab <112143246+yamanAlrajab@users.noreply.github.com>
2024-03-18 13:04:14 -0400

    Update students.sql

--- Assignment3q1/students.sql
+++ Assignment3q1/students.sql
@@ -5,7 +5,7 @@ create table students(
 	email text not null unique,
 	enrollment_date Date
 );
-INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
-('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+INSERT INTO students (first_name, last_name, email, enrollment_date)
+VALUES('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
 ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
